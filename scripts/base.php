a33d0c164b2a1c8f4e8fd6eefea8f32e715625a8
maha2895 <maha2895@colorado.edu>
2018-03-05 13:08:27 -0700

    updated readme with apache info

--- scripts/base.php
+++ scripts/base.php
@@ -9,10 +9,18 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 session_start();
 
-$dbhost = "localhost";
-$dbname = "buildit";
-$dbuser = "admin";
-$dbpass = "password";
+// ========= FOR STAGING AND PRODUCTION =============
+$dbhost = "fdb19.awardspace.net";
+$dbname = "2642413_buildit";
+$dbuser = "2642413_buildit";
+$dbpass = "password1";
+$dbport = "3306";
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysqli_connect_error());
+// ========= FOR LOCAL TESTING ===========
+//$dbhost = "localhost";
+//$dbname = "buildit";
+//$dbuser = "admin";
+//$dbpass = "password";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport) or die("MySQL Error: " . mysqli_connect_error());
 mysqli_select_db($conn, $dbname) or die("MySQL Error: " . mysqli_error($conn));
