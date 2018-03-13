65c470723aa013abdede393564f3d4864fccefce
Ali Noor <36457865+AliNoor1@users.noreply.github.com>
2018-03-12 18:14:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 <html>
 	<head>
 		<link href="/css/BuildIT_style.css" type="text/css" rel="stylesheet">
-        <link href="/css/navbar.css" type="text/css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
+       		<link href="/css/navbar.css" type="text/css" rel="stylesheet">
+       	 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    	</head>
 
 	<title>BuildIT</title>
 	
 	<body>
 		<!-- HEADER -->
-		<div class= "topContainer">
+		<div id= "topContainer">
 		
 			<!-- BuildIT Logo/Box -->
 			<div class="header">
@@ -23,17 +23,24 @@
 		</div>
 		<!-- FIRST IMAGE ON THE SITE -->
 		<div class="secondaryContainer">
-			<img class= "imagePreview" src="img/home01.jpg" alt="Shed_img">
+			
+		<div class= "slideShow">
+			<div class="slide"><img class= "imagePreview" src="img/home01.jpg" alt="Shed_img"></div>
+			<div class="slide"><img class= "imagePreview" src="img/home01.jpg" alt="Shed_img"></div>
+			<div class="slide"><img class= "imagePreview" src="img/home01.jpg" alt="Shed_img"></div>
+			<div class="slide"><img class= "imagePreview" src="img/home01.jpg" alt="Shed_img"></div>
+			
 			<div class="imageTab">
 				<div class="shutterOutTab">
 				<a href="#">Get Started</a>
 				</div>
 			</div>
 		</div>
+		</div>
 		
-		<div class= "description">
+		<div id= "description">
 			<h2 id= "desc_header">BuildIT Sets Out A Plan For Anyone, Anywhere</h2>
-			<p>fdinfsadfbnm fsfdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd fd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd f </p> 
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricies vulputate odio, et pulvinar ante pellentesque ac. Nullam placerat quam sit amet aliquet dictum. Duis mattis nisl faucibus tellus maximus, sit amet fringilla tortor interdum. Vivamus ex lorem, tincidunt sit amet dolor at, vestibulum eleifend nulla. Vestibulum tempus nunc orci, quis ornare turpis congue at. Maecenas in enim mi. Nunc sit amet leo eu libero molestie volutpat. Nunc faucibus ligula sed tortor finibus hendrerit. Sed nec enim vitae risus aliquam tristique. Curabitur ullamcorper, neque sed tempor dignissim, lorem nisi faucibus augue, sed tristique lacus nulla ac dolor.</p> 
 		
 		</div>
 		
@@ -47,9 +54,9 @@
 			</div>
 		</div>
 		
-		<div class= "description">
+		<div id= "description">
 			<h2 id= "desc_header">Find A Contractor Today</h2>
-			<p>fdinfsadfbnm fsfdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd fd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd f </p> 
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricies vulputate odio, et pulvinar ante pellentesque ac. Nullam placerat quam sit amet aliquet dictum. Duis mattis nisl faucibus tellus maximus, sit amet fringilla tortor interdum. Vivamus ex lorem, tincidunt sit amet dolor at, vestibulum eleifend nulla. Vestibulum tempus nunc orci, quis ornare turpis congue at. Maecenas in enim mi. Nunc sit amet leo eu libero molestie volutpat. Nunc faucibus ligula sed tortor finibus hendrerit. Sed nec enim vitae risus aliquam tristique. Curabitur ullamcorper, neque sed tempor dignissim, lorem nisi faucibus augue, sed tristique lacus nulla ac dolor.</p> 
 		
 		</div>
 		
@@ -63,9 +70,9 @@
 			</div>
 		</div>
 		
-		<div class= "description">
+		<div id= "description">
 			<h2 id= "desc_header">A Community Of Builders</h2>
-			<p>fdinfsadfbnm fsfdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd ffdinfsadfbnm fsd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd fd fs df sdf s df sdf f sd f sdf sdf sdf sd f sdf s df sdf s df sdf sdf  sfd f a fdas f asd fa sd f </p> 
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricies vulputate odio, et pulvinar ante pellentesque ac. Nullam placerat quam sit amet aliquet dictum. Duis mattis nisl faucibus tellus maximus, sit amet fringilla tortor interdum. Vivamus ex lorem, tincidunt sit amet dolor at, vestibulum eleifend nulla. Vestibulum tempus nunc orci, quis ornare turpis congue at. Maecenas in enim mi. Nunc sit amet leo eu libero molestie volutpat. Nunc faucibus ligula sed tortor finibus hendrerit. Sed nec enim vitae risus aliquam tristique. Curabitur ullamcorper, neque sed tempor dignissim, lorem nisi faucibus augue, sed tristique lacus nulla ac dolor.</p> 
 		
 		</div>
 		
