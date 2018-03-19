d2afbc7d2486d279a2ccabf91d30b35fc89cee55
Ali Noor <36457865+AliNoor1@users.noreply.github.com>
2018-03-19 11:24:02 -0600

    Update navbar.php

--- common/navbar.php
+++ common/navbar.php
@@ -1,23 +1,23 @@
-<!-- NAVIGATION BAR -->
+	<!-- NAVIGATION BAR -->
 <div class="topnav">
-    <a style="background-color:#3c4b68;" href="/">
+    <a class="homeButton" href="/">
         <i class="fa fa-home"></i>
     </a>
-    <a href="#">Design</a>
-    <a href="#">Forums</a>
-    <a href="#">Contracts</a>
+    <a class="topLinks" href="#">Design</a>
+    <a class="topLinks" href="#">Forums</a>
+    <a class="topLinks" href="#">Contracts</a>
 
     <?php
     if (!empty($_SESSION['LoggedIn']))
     {
-        echo "<a style='float:right' href='/user/index.php?username=".$_SESSION['username']."'>".$_SESSION['firstname']."</a>";
-        echo "<a style='float:right' href='/logout/index.php'>Logout</a>";
+        echo "<a class="topLinks" style='float:right' href='/user/index.php?username=".$_SESSION['username']."'>".$_SESSION['firstname']."</a>";
+        echo "<a class="topLinks" style='float:right' href='/logout/index.php'>Logout</a>";
     }
     else{
-        echo "<a style='float:right' href='/login/index.php'>Login/Sign Up</a>";
+        echo <a class="login_signup" href='/login/index.php'><i class="fa fa-user-circle"></i></a>;
     }
-
     ?>
+	
     <div class="search-container">
         <form method="get" action="/search/" id="searchform">
             <input type="text" placeholder="Search..." name="query">
