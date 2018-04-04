0425678774a1d5ef94c1fef338703f4fe3bf7f04
Ali Noor <36457865+AliNoor1@users.noreply.github.com>
2018-04-04 15:44:38 -0600

    Update index.php

--- forum/index.php
+++ forum/index.php
@@ -1,18 +1,37 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/forum/header.php"; ?>
 
-<h1>Forum</h1>
 
-<div id="wrapper">
-    <div id="menu">
+<!DOCTYPE html>
+
+<html>
+<head>
+	<link href="../css/BuildIT_Forum_style.css" type="text/css" rel="stylesheet">
+	<link href="../css/navbar.css" type="text/css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<h1 class="header">Forums</h1>
+
+	<div class="search-forum">
+        <form method="get" action="/search/" id="searchform">
+            <input type="text" placeholder="Search..." name="query">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+    </div>
+
+<div class="wrapper">
+    <div class="menu">
         <a class="item" href="/forum/index.php">Home</a> -
-        <a class="item" href="/forum/create_topic.php">Create a topic</a> -
-        <a class="item" href="/forum/create_cat.php">Create a category</a>
+        <a class="item" href="/forum/create_topic.php">Create Topic</a> -
+        <a class="item" href="/forum/create_cat.php">Create Category</a>
 
     </div>
 
     <hr>
 
-    <div id="content">
+    <div class="content">
         <?php
         $sql = "SELECT
             cat_id,
@@ -37,10 +56,12 @@
             {
                 ?>
 <!--                //prepare the table-->
-              <table border="1">
+              <table class="ftable">
               <tr>
                 <th>Category</th>
-                <th>Last topic</th>
+                <th>Topic</th>
+				<th>Posts</th>
+				<th>Author</th>
               </tr>
             <?php
 
@@ -48,12 +69,18 @@
                 {
                     ?>
                     <tr>
-                        <td class="leftpart">
-                            <h3><a href="category.php?id=<?=$row['cat_id']?>"><?=$row['cat_name']?></a></h3>
+                        <td class="category_row">
+                            <a href="category.php?id=<?=$row['cat_id']?>"><?=$row['cat_name']?> Workshops</a>
                             <?=$row['cat_description']?>
                         </td>
-                        <td class="rightpart">
+                        <td class="topic_row">
                             <a href="topic.php?id=">Topic subject</a>
+                        </td>
+						<td class="posts_row">
+                            <a href="#">Number of posts</a>
+                        </td>
+						<td class="author_row">
+                            <a href="#"><img class= "imageForum" src="https://www.weact.org/wp-content/uploads/2016/10/Blank-profile.png" alt="profile_img"> John Doe</a>
                         </td>
                     </tr>
         <?php
@@ -63,4 +90,10 @@
         ?>
     </div>
 </div>
+
+</body>
+
+</html>
+
+
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/forum/footer.php"; ?>
