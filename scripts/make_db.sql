299bca901bb805900e79079703910ba72b03a055
maha2895 <maha2895@colorado.edu>
2018-04-06 14:37:56 -0600

    updated make_db.sql

--- scripts/make_db.sql
+++ scripts/make_db.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS forum_categories (
 cat_id          INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 cat_name        VARCHAR(255) NOT NULL,
 cat_description     VARCHAR(255) NOT NULL,
-UNIQUE INDEX cat_name_unique (cat_name),
+UNIQUE INDEX cat_name_unique (cat_name)
 );
 
 CREATE TABLE IF NOT EXISTS forum_topics (
@@ -49,7 +49,7 @@ topic_id        INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 topic_subject       VARCHAR(255) NOT NULL,
 topic_date      DATETIME NOT NULL,
 topic_cat       INT(8) NOT NULL,
-topic_by        INT(8) NOT NULL,
+topic_by        INT(8) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS forum_posts (
@@ -57,7 +57,7 @@ post_id         INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 post_content        TEXT NOT NULL,
 post_date       DATETIME NOT NULL,
 post_topic      INT(8) NOT NULL,
-post_by     INT(8) NOT NULL,
+post_by     INT(8) NOT NULL
 );
 
 -- Linking topics and categories
@@ -84,3 +84,4 @@ ALTER TABLE forum_posts
   ADD FOREIGN KEY(post_by)
   REFERENCES users(userID)
   ON DELETE RESTRICT ON UPDATE CASCADE;
+  
